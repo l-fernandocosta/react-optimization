693f406d5d338f988ded72d636a995468951405e
l-fernandocosta <fernandocostadev98@gmail.com>
2022-03-04 07:22:02 -0300

    alteration index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,10 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const [input, setInput] = useState('');
-
-
   function handleSubmit(){
       
     
   }
-  
   
   return (
     <div className={styles.container}>
